3bbd6671711d5410df94f43d97c33d04ba49f88d
Vladimir <fovlvl.0702@gmail.com>
2023-11-06 19:20:09 +0300

    Project 2 - solve

--- src/test/java/edu/project2_maze/mazeSolver/MazeSolverRandomTest.java
+++ src/test/java/edu/project2_maze/mazeSolver/MazeSolverRandomTest.java
@@ -11,8 +11,6 @@ class MazeSolverRandomTest {
 
     @Test
     void solve() {
-        MazeSession maze = new MazeSession();
-
         Cell[][] input = {  {new Cell(0, 0, WALL), new Cell(0, 1, WALL), new Cell(0, 3, WALL), new Cell(0, 4, WALL), new Cell(0, 5, WALL) },
                             {new Cell(1, 0, WALL), new Cell(1, 1, PASSAGE), new Cell(1, 3, PASSAGE), new Cell(1, 4, END_POS), new Cell(1, 5, WALL) },
                             {new Cell(2, 0, WALL), new Cell(2, 1, PASSAGE), new Cell(2, 3, WALL), new Cell(2, 4, WALL), new Cell(2, 5, WALL) },
