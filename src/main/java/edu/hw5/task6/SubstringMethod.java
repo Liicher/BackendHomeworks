6f910d27a5a6974069f49365872e9f793fe72814
Vladimir <fovlvl.0702@gmail.com>
2023-11-08 21:37:41 +0300

    task 6 - checkstyle

--- src/main/java/edu/hw5/task6/SubstringMethod.java
+++ src/main/java/edu/hw5/task6/SubstringMethod.java
@@ -4,13 +4,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SubstringMethod {
-    public boolean isSubstring(String S, String T) {
-        if (S == null || T == null || T.isEmpty()) {
+    public boolean isSubstring(String subStringInput, String fullStringInput) {
+        if (subStringInput == null || fullStringInput == null || fullStringInput.isEmpty()) {
             throw new IllegalArgumentException();
         }
 
-        Pattern subStringPattern = Pattern.compile(".*" + S + ".*");
-        Matcher stringMatcher = subStringPattern.matcher(T);
+        Pattern subStringPattern = Pattern.compile(".*" + subStringInput + ".*");
+        Matcher stringMatcher = subStringPattern.matcher(fullStringInput);
         return stringMatcher.find();
     }
 }
