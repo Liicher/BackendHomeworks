34c58ba7f19c047de11662485cc98ece9344c8d1
Vladimir <fovlvl.0702@gmail.com>
2023-11-16 01:17:09 +0300

    redid test for git

--- src/main/java/edu/hw6/task6/PortScanner.java
+++ src/main/java/edu/hw6/task6/PortScanner.java
@@ -3,9 +3,8 @@ package edu.hw6.task6;
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.ServerSocket;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,30 +20,40 @@ public class PortScanner {
     private final static Logger LOGGER = LogManager.getLogger();
     private final static int PORT_MIN = 0;
     private final static int PORT_MAX = 49151;
+    private final static Map<Integer, String> POPULAR_PORTS = Map.of(
+        135, "EPMAP",
+        137, "Служба имен NetBIOS",
+        139, "Служба сеансов NetBIOS",
+        445, "Microsoft-DS Active Directory",
+        843, "Adobe Flash",
+        1900, "SSDP",
+        3702, "Динамическое обнаружение веб-служб",
+        5353, "Многоадресный DNS",
+        17500, "Dropbox",
+        27017, "MongoDB"
+    );
 
-    // Буду возвращать мапу
-    // Ключ - TCP/UDP + приложение
-    // Значение - порт
-    public Map<List<String>, Integer> scan(Map<Integer, String> inputPorts) {
-        Map<List<String>, Integer> result = new HashMap<>();
+    // Возвращаю количество проверенных портов, которые имеют приложение, которое их использует
+    public int scan(Set<Integer> inputPorts) {
+        int inputPortScannedCounter = 0;
         for (int port = PORT_MIN; port <= PORT_MAX; port++) {
-            String portService = inputPorts.getOrDefault(port, "");
+            String portService = POPULAR_PORTS.getOrDefault(port, "");
+            int popularPort = inputPorts.contains(port) ? port : -1;
 
-            if (!portService.isEmpty()) {
+            if (!portService.isEmpty() && popularPort != -1) {
                 // TCP
                 try (ServerSocket serverSocket = new ServerSocket(port)) {
                     LOGGER.info("TCP\t" + port + "\t" + portService);
-                    result.put(List.of("TCP", portService), port);
                 } catch (IOException ignored) {
                 }
                 // UDP
                 try (DatagramSocket datagramSocket = new DatagramSocket(port)) {
                     LOGGER.info("UDP\t" + port + "\t" + portService);
-                    result.put(List.of("UDP", portService), port);
                 } catch (IOException ignored) {
                 }
+                inputPortScannedCounter++;
             }
         }
-        return result;
+        return inputPortScannedCounter;
     }
 }
--- src/test/java/edu/hw6/task6/PortScannerTest.java
+++ src/test/java/edu/hw6/task6/PortScannerTest.java
@@ -1,53 +1,29 @@
 package edu.hw6.task6;
 
-import org.junit.jupiter.api.BeforeAll;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PortScannerTest {
-    private Map<List<String>, Integer> expectedMap;
-    private Map<Integer, String> portsMap;
+    private Set<Integer> portsMap;
 
     @BeforeEach
     void init() {
-        portsMap = Map.of(
-            135, "EPMAP",
-            137, "Служба имен NetBIOS",
-            139, "Служба сеансов NetBIOS",
-            445, "Microsoft-DS Active Directory",
-            843, "Adobe Flash",
-            1900, "SSDP",
-            3702, "Динамическое обнаружение веб-служб",
-            5353, "Многоадресный DNS",
-            17500, "Dropbox",
-            27017, "MongoDB");
-
-        expectedMap = new HashMap<>();
-        expectedMap.put(List.of("UDP", "EPMAP"), 135);
-        expectedMap.put(List.of("TCP", "Служба имен NetBIOS"), 137);
-        expectedMap.put(List.of("UDP", "Служба сеансов NetBIOS"), 139);
-        expectedMap.put(List.of("UDP", "Microsoft-DS Active Directory"), 445);
-        expectedMap.put(List.of("TCP", "Adobe Flash"), 843);
-        expectedMap.put(List.of("UDP", "Adobe Flash"), 843);
-        expectedMap.put(List.of("TCP", "SSDP"), 1900);
-        expectedMap.put(List.of("TCP", "Динамическое обнаружение веб-служб"), 3702);
-        expectedMap.put(List.of("TCP", "Многоадресный DNS"), 5353);
-        expectedMap.put(List.of("TCP", "Dropbox"), 17500);
-        expectedMap.put(List.of("UDP", "Dropbox"), 17500);
-        expectedMap.put(List.of("TCP", "MongoDB"), 27017);
-        expectedMap.put(List.of("UDP", "MongoDB"), 27017);
+        portsMap = Set.of(135, 137, 139, 445, 843, 1900, 3702, 5353, 17500, 27017);
     }
 
     @Test
     void scanPorts() {
         PortScanner portScanner = new PortScanner();
-        Map<List<String>, Integer>  response = portScanner.scan(portsMap);
-        assertThat(response).isEqualTo(expectedMap);
+        int response = portScanner.scan(portsMap);
+        assertThat(response).isEqualTo(portsMap.size());
+
+        response = portScanner.scan(Set.of());
+        assertThat(response).isEqualTo(0);
+
+        // Этого порта нет в нашей библиотеке "популярных" портов, поэтому мы не можем узнать кем он используется
+        response = portScanner.scan(Set.of(111));
+        assertThat(response).isEqualTo(0);
     }
 }
