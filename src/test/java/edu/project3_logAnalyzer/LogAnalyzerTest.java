f903f8e3a40607bf080d73f7a79be81ed721ccc6
Vladimir <fovlvl.0702@gmail.com>
2023-11-19 19:26:08 +0300

    add adoc test

--- src/test/java/edu/project3_logAnalyzer/LogAnalyzerTest.java
+++ src/test/java/edu/project3_logAnalyzer/LogAnalyzerTest.java
@@ -8,25 +8,44 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LogAnalyzerTest {
+    private String testLogPath = null;
     private LogAnalyzer logAnalyzer;
-    private Path path;
-    private File file;
+    private Path pathToMd;
+    private Path pathToAdoc;
+    private File fileMd;
+    private File fileAdoc;
 
     @BeforeEach
     void init() {
         logAnalyzer = new LogAnalyzer();
-        path = Path.of("src/main/java/edu/project3_logAnalyzer/outputs/output.md");
-        file = path.toFile();
+        testLogPath = "src/test/java/edu/project3_logAnalyzer/testLog.txt";
+
+        pathToMd = Path.of("src/main/java/edu/project3_logAnalyzer/outputs/output.md");
+        fileMd = pathToMd.toFile();
+
+        pathToAdoc = Path.of("src/main/java/edu/project3_logAnalyzer/outputs/output.adoc");
+        fileAdoc = pathToAdoc.toFile();
     }
 
     @Test
     void markdownOutput() {
         logAnalyzer.analyze(new String[] {
             "java","-jar","nginx-log-stats.jar","--path",
-            "src/test/java/edu/project3_logAnalyzer/testLog.txt",
-            "--from","2015-05-25","--format", "markdown"});
+            testLogPath, "--from","2015-05-25","--format", "markdown"
+        });
 
-        assertThat(file).isFile();
-        assertThat(file.getName()).isEqualTo("output.md");
+        assertThat(fileMd).isFile();
+        assertThat(fileMd.getName()).isEqualTo("output.md");
+    }
+
+    @Test
+    void adocOutput() {
+        logAnalyzer.analyze(new String[] {
+            "java","-jar","nginx-log-stats.jar","--path",
+            testLogPath, "--from","2015-05-25","--format", "adoc"
+        });
+
+        assertThat(fileAdoc).isFile();
+        assertThat(fileAdoc.getName()).isEqualTo("output.adoc");
     }
 }
