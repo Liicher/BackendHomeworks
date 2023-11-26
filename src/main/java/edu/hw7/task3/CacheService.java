61e60df199be3e2cfa365ec019d42a56c6f4f5c5
Vladimir <fovlvl.0702@gmail.com>
2023-11-27 00:33:37 +0300

    last remark

--- src/main/java/edu/hw7/task3/CacheService.java
+++ src/main/java/edu/hw7/task3/CacheService.java
@@ -16,15 +16,42 @@ public class CacheService implements PersonDatabase {
             throw new IllegalArgumentException();
         }
         idCache.put(person.id(), person);
-        nameCache.put(person.name(), person);
-        addressCache.put(person.address(), person);
-        phoneCache.put(person.phoneNumber(), person);
+        putName(person.id(), person.name());
+        putAddress(person.id(), person.address());
+        putPhone(person.id(), person.phoneNumber());
+    }
+
+    // Классы для добавления/изменения данных в кэшах
+    public synchronized void putName(int id, String name) {
+        Person person = idCache.get(id);
+        if (nameCache.containsKey(name)) {
+            throw new IllegalArgumentException();
+        } else {
+            nameCache.put(name, person);
+        }
+    }
+
+    public synchronized void putAddress(int id, String address) {
+        Person person = idCache.get(id);
+        if (addressCache.containsKey(address)) {
+            throw new IllegalArgumentException();
+        } else {
+            addressCache.put(address, person);
+        }
+    }
+
+    public synchronized void putPhone(int id, String phoneNumber) {
+        Person person = idCache.get(id);
+        if (phoneCache.containsKey(phoneNumber)) {
+            throw new IllegalArgumentException();
+        } else {
+            phoneCache.put(phoneNumber, person);
+        }
     }
 
     @Override
     public synchronized void delete(int id) {
         Person person = idCache.get(id);
-
         nameCache.remove(person.name());
         addressCache.remove(person.address());
         phoneCache.remove(person.phoneNumber());
