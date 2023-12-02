6b9db0c99fb5ae3404d58c7ae64f843baa66e042
Vladimir <fovlvl.0702@gmail.com>
2023-12-02 22:45:38 +0300

    project 4 - init

--- src/main/java/edu/project4_fractal_flame/Main.java
+++ src/main/java/edu/project4_fractal_flame/Main.java
@@ -1,2 +1,10 @@
-package edu.project4_fractal_flame;public class Main {
+package edu.project4_fractal_flame;
+
+public class Main {
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
