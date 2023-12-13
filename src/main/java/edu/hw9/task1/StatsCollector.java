95c9845d9867fbf2124f7f9476be0e382cc51133
Vladimir <fovlvl.0702@gmail.com>
2023-12-13 22:41:37 +0300

    project 4 - fix collection and locking remarks

--- src/main/java/edu/hw9/task1/StatsCollector.java
+++ src/main/java/edu/hw9/task1/StatsCollector.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -25,7 +26,7 @@ public class StatsCollector {
 
     public StatsCollector(int amountOfThreads) {
         this.executorService = Executors.newFixedThreadPool(amountOfThreads);
-        this.metrics = new HashMap<>();
+        this.metrics = new ConcurrentHashMap<>();
     }
 
     public void push(String metricName, float[] data) {
@@ -44,25 +45,27 @@ public class StatsCollector {
 
     public Map<String, Float> stats() {
         Map<String, Float> stats = new TreeMap<>();
-        for (String metricName : metrics.keySet()) {
-            List<Float> data = metrics.get(metricName);
-            float sum = 0;
-            float min = Float.MAX_VALUE;
-            float max = Float.MIN_VALUE;
-            for (float d : data) {
-                sum += d;
-                if (d < min) {
-                    min = d;
+        synchronized (metrics) {
+            for (String metricName : metrics.keySet()) {
+                List<Float> data = metrics.get(metricName);
+                float sum = 0;
+                float min = Float.MAX_VALUE;
+                float max = Float.MIN_VALUE;
+                for (float d : data) {
+                    sum += d;
+                    if (d < min) {
+                        min = d;
+                    }
+                    if (d > max) {
+                        max = d;
+                    }
                 }
-                if (d > max) {
-                    max = d;
-                }
+                float average = sum / data.size();
+                stats.put(metricName + "_sum", sum);
+                stats.put(metricName + "_average", average);
+                stats.put(metricName + "_min", min);
+                stats.put(metricName + "_max", max);
             }
-            float average = sum / data.size();
-            stats.put(metricName + "_sum", sum);
-            stats.put(metricName + "_average", average);
-            stats.put(metricName + "_min", min);
-            stats.put(metricName + "_max", max);
         }
         return stats;
     }
